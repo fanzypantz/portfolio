28d5cbac85cff5cc7cb0f409dd3a3d6853598dd1
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-04-16 18:50:55 +0200

    Update Lighting.tsx

--- src/components/3D/Lighting.tsx
+++ src/components/3D/Lighting.tsx
@@ -1,33 +1,14 @@
 "use client";
 
-import { createRef, memo, RefObject, useRef } from "react";
-import {
-  AccumulativeShadows,
-  RandomizedLight,
-  PivotControls,
-  Environment as EnvironmentImpl,
-  ContactShadows
-} from "@react-three/drei";
-import { DirectionalLight, DirectionalLightHelper } from "three";
+import { Environment as EnvironmentImpl } from "@react-three/drei";
 
 const Lighting = () => {
-  // const light: RefObject<DirectionalLight> | undefined = createRef<DirectionalLight>();
-  // useHelper(light, DirectionalLightHelper, "cyan");
-
   return (
     <group position={[0, 0, 0]}>
-      <directionalLight position={[-15, 15, 15]} intensity={0.5} shadow-mapSize={1024} castShadow />
-      <directionalLight position={[15, 5, 15]} intensity={0.5} shadow-mapSize={1024} castShadow />
+      <directionalLight position={[-15, 15, 15]} intensity={1} shadow-mapSize={1024} castShadow />
+      <directionalLight position={[15, 5, 15]} intensity={0.1} shadow-mapSize={1024} castShadow />
 
-      {/*<PivotControls scale={1}>*/}
-      {/*  <AccumulativeShadows  frames={100} alphaTest={0.85} opacity={0.75} scale={30} position={[0, -1.5, 0]}>*/}
-      {/*    <RandomizedLight amount={8} radius={2.5} ambient={0.5} intensity={1} position={direction} bias={0.001} />*/}
-      {/*  </AccumulativeShadows>*/}
-      {/*</PivotControls>*/}
-
-      <ContactShadows position={[0, -0.4, 0]} opacity={0.2} blur={1} scale={8} />
-
-      <EnvironmentImpl preset="city" />
+      <EnvironmentImpl preset="sunset" />
     </group>
   );
 };
