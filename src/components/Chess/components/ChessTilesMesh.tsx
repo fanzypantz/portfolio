ea40cf5a8fca1fdeb4a780c5c898b6bedf59f896
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-04-17 22:00:45 +0200

    Update ChessTilesMesh.tsx

--- src/components/Chess/components/ChessTilesMesh.tsx
+++ src/components/Chess/components/ChessTilesMesh.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Game } from "@components/Chess/Game";
+import { Game } from "@components/BoardGame/Game";
 import ChessTileMesh from "@components/Chess/components/ChessTileMesh";
 
 const ChessTilesMesh = ({ chessGame }: { chessGame: Game }) => {
