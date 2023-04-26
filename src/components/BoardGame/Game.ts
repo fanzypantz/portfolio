4f06e2980e32424d878a1dc64d10c8cdf16fee86
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-04-26 23:09:42 +0200

    Update Game.ts

--- src/components/BoardGame/Game.ts
+++ src/components/BoardGame/Game.ts
@@ -2,15 +2,13 @@ import { Position } from "@components/BoardGame/Position";
 import { Board } from "@components/BoardGame/Board";
 import { Player } from "@components/BoardGame/Player";
 import { Move, AbstractMove } from "@components/BoardGame/Move";
+import { GameScore } from "@components/BoardGame/GameScore";
 
 export interface Game {
   get board(): Board;
   get players(): Player[];
   get currentPlayer(): Player;
   get winner(): Player | undefined;
-  get isOver(): boolean;
-  get isDraw(): boolean;
-  get isStalemate(): boolean;
   movePiece(from: Position, to: Position): void;
   undoMove(): void;
   redoMove(): void;
@@ -21,11 +19,13 @@ export class AbstractGame implements Game {
   private readonly gameBoard: Board;
   private readonly gamePlayers: Player[];
   private readonly gameMoves: Move[];
+  private readonly gameScore: GameScore;
   private gameMovesIndex: number;
 
-  constructor(gameBoard: Board, gamePlayers: Player[]) {
+  constructor(gameBoard: Board, gamePlayers: Player[], gameScore: GameScore) {
     this.gameBoard = gameBoard;
     this.gamePlayers = gamePlayers;
+    this.gameScore = gameScore;
     this.gameMoves = [];
     this.gameMovesIndex = 0;
   }
@@ -43,18 +43,6 @@ export class AbstractGame implements Game {
   }
 
   public get winner(): Player | undefined {
-    throw new Error("Method not implemented.");
-  }
-
-  public get isOver(): boolean {
-    throw new Error("Method not implemented.");
-  }
-
-  public get isDraw(): boolean {
-    throw new Error("Method not implemented.");
-  }
-
-  public get isStalemate(): boolean {
     throw new Error("Method not implemented.");
   }
 
