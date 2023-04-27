a671af7791c4450ae04ff041b20217cb389a2dce
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-04-27 18:05:05 +0200

    Update Renderer.tsx

--- src/components/3D/Renderer.tsx
+++ src/components/3D/Renderer.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import styles from "@styles/Renderer.module.scss";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
+import { useRef, useState } from "react";
+import { Mesh } from "three";
 
 const Renderer = ({ children }: { children?: JSX.Element | JSX.Element[] }) => {
   return (
     <div className={styles.canvas}>
-      <Canvas camera={{ position: [0, 2, 4] }} shadows>
+      <Canvas style={{ width: "100%", height: "100%" }} camera={{ position: [0, 2, 4] }} shadows>
         {children}
       </Canvas>
     </div>
