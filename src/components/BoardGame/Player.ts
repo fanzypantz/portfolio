421cfab7dc511b058044d58e750b347ae22de54f
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-04-27 18:04:17 +0200

    Update Player.ts

--- src/components/BoardGame/Player.ts
+++ src/components/BoardGame/Player.ts
@@ -1,15 +1,15 @@
-import { Color } from "@components/BoardGame/Color";
+import { PieceColor } from "@components/BoardGame/Piece";
 
 export interface Player {
   get name(): string;
-  get color(): Color;
+  get color(): PieceColor;
 }
 
 export class AbstractPlayer implements Player {
   private readonly playerName: string;
-  private readonly playerColor: Color;
+  private readonly playerColor: PieceColor;
 
-  constructor(name: string, color: Color) {
+  constructor(name: string, color: PieceColor) {
     this.playerName = name;
     this.playerColor = color;
   }
@@ -18,7 +18,7 @@ export class AbstractPlayer implements Player {
     return this.playerName;
   }
 
-  public get color(): Color {
+  public get color(): PieceColor {
     return this.playerColor;
   }
 }
