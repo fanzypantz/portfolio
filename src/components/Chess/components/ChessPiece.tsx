b747fdafcc49af5f8d5fa7f13baf688b28356101
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-05-30 21:21:04 +0200

    Removed the click and drag from pieces added select piece

--- src/components/Chess/components/ChessPiece.tsx
+++ src/components/Chess/components/ChessPiece.tsx
@@ -1,37 +1,62 @@
 "use client";
 
-import { ThreeEvent, useLoader, useThree } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 import StandardMesh from "@components/3D/StandardMesh";
 import { Piece, PieceColor } from "@components/BoardGame/Piece";
-import { Camera, Mesh, Vector2, Vector3 } from "three";
-import { black, hover, white } from "@components/BoardGame/Materials";
-import { PointerEvent, useRef, useState } from "react";
-import useCursor from "@components/BoardGame/UseCursor";
+import { Vector3 } from "three";
+import { black, hover, selected, white } from "@components/BoardGame/Materials";
+import { useContext, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { ChessContext } from "@components/Chess/components/chessProvider";
 
-const ChessPiece = ({ piece }: { piece: Piece }) => {
-  const { selected, handleClick, meshRef } = useCursor();
+const ChessPiece = observer(({ piece, onPieceClick }: { piece: Piece; onPieceClick?: (piece: Piece) => void }) => {
   const gltf = useLoader(GLTFLoader, `/models/${piece.type}.glb`);
+  const { chessGame } = useContext(ChessContext);
 
-  const [hovered, setHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const onHover = (e: any, value: boolean) => {
     e.stopPropagation();
-    setHovered(value);
+    setIsHovered(value);
   };
+
+  const onClick = () => {
+    handlePieceClick();
+    if (onPieceClick) {
+      onPieceClick(piece);
+    }
+  };
+
+  const handlePieceClick = () => {
+    if (chessGame?.board.selectedPiece?.position.equals(piece.position)) {
+      chessGame?.board.unselectPiece();
+    } else {
+      chessGame?.board.selectPiece(piece);
+    }
+  };
+
+  const getMaterial = () => {
+    if (chessGame?.board.selectedPiece?.position.equals(piece.position)) {
+      return selected;
+    } else if (isHovered) {
+      return hover;
+    } else {
+      return piece.color === PieceColor.White ? white : black;
+    }
+  };
+
   return (
     <StandardMesh
-      // @ts-ignore
-      meshRef={meshRef}
       onPointerOver={(e) => onHover(e, true)}
       onPointerOut={(e) => onHover(e, false)}
-      onClick={handleClick}
+      onClick={() => onClick()}
       gltf={gltf}
-      position={new Vector3(piece.position.currentPosition.y / 5, 0, piece.position.currentPosition.x / 5)}
-      material={hovered ? hover : piece.color === PieceColor.White ? white : black}
+      position={new Vector3(piece.position.currentPosition.y / 5 - 0.7, 0, piece.position.currentPosition.x / 5 - 0.7)}
+      material={getMaterial()}
     />
   );
-};
+});
 
 export default ChessPiece;
