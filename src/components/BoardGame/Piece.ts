6d8fc35173431ef9c404e2b758fe1f1a43fe453c
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2023-05-30 21:19:17 +0200

    Added observables to the Piece

--- src/components/BoardGame/Piece.ts
+++ src/components/BoardGame/Piece.ts
@@ -1,6 +1,7 @@
 import { Move } from "@components/BoardGame/Move";
 import { Position } from "@components/BoardGame/Position";
 import { Board } from "@components/BoardGame/Board";
+import { action, makeObservable, observable } from "mobx";
 
 export enum PieceColor {
   White = "White",
@@ -10,46 +11,48 @@ export enum PieceColor {
 export interface Piece {
   get type(): string;
   get color(): PieceColor;
-  get position(): Position;
-  get possibleMoves(): Move[];
-  set position(position: Position);
+  position: Position;
+  possibleMoves: Move[];
+  setPosition(position: Position): void;
   generatePossibleMoves(board: Board): void;
+  toString(): string;
 }
 
 export class AbstractPiece implements Piece {
-  private readonly pieceType: string;
-  private readonly pieceColor: PieceColor;
-  private piecePosition: Position;
-  private readonly possibleMovesList: Move[];
+  private readonly _type: string;
+  private readonly _color: PieceColor;
+  position: Position;
+  possibleMoves: Move[] = [];
 
   constructor(type: string, color: PieceColor, position: Position) {
-    this.pieceType = type;
-    this.pieceColor = color;
-    this.piecePosition = position;
-    this.possibleMovesList = [];
+    this._type = type;
+    this._color = color;
+    this.position = position;
+
+    makeObservable(this, {
+      position: observable,
+      possibleMoves: observable,
+      setPosition: action
+    });
   }
 
   get type(): string {
-    return this.pieceType;
+    return this._type;
   }
 
   get color(): PieceColor {
-    return this.pieceColor;
+    return this._color;
   }
 
-  get position(): Position {
-    return this.piecePosition;
-  }
-
-  get possibleMoves(): Move[] {
-    return this.possibleMovesList;
-  }
-
-  set position(position: Position) {
-    this.piecePosition = position;
+  setPosition(position: Position) {
+    this.position = position;
   }
 
   generatePossibleMoves(board: Board): void {
     throw new Error("Method not implemented.");
+  }
+
+  toString(): string {
+    return `${this.color} ${this.type}`;
   }
 }
