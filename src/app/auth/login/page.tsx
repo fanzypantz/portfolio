5ba6331d0a1bb57d744910a886ff13c5992daff2
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2024-01-12 18:37:22 +0100

    Update page.tsx

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@supabase/database.types";
 import { cookies } from "next/headers";
+import GoogleButton from "@components/Auth/GoogleButton";
 
 const url = "http://localhost:3000";
 
@@ -12,10 +13,12 @@ export default function Page() {
     const password = String(formData.get("password"));
     const supabase = createRouteHandlerClient<Database>({ cookies });
 
-    await supabase.auth.signInWithPassword({
+    const { data, error } = await supabase.auth.signInWithPassword({
       email,
       password
     });
+
+    console.log("data : ", data, error);
   };
 
   const signup = async (formData: FormData) => {
@@ -25,23 +28,29 @@ export default function Page() {
     const password = String(formData.get("password"));
     const supabase = createRouteHandlerClient<Database>({ cookies });
 
-    await supabase.auth.signUp({
+    const { data, error } = await supabase.auth.signUp({
       email,
       password,
       options: {
         emailRedirectTo: `${url}/auth/callback`
       }
     });
+
+    console.log("data : ", data, error);
   };
 
   return (
-    <form action={login}>
-      <label htmlFor="email">Email</label>
-      <input name="email" defaultValue={"dm@andreas-tollanes.com"} />
-      <label htmlFor="password">Password</label>
-      <input type="password" name="password" defaultValue={"123123"} />
-      <button>Sign In</button>
-      <button formAction={signup}>Sign Up</button>
-    </form>
+    <div>
+      <form action={login}>
+        <label htmlFor="email">Email</label>
+        <input name="email" defaultValue={"dm@andreas-tollanes.com"} />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" defaultValue={"123123"} />
+        <button>Sign In</button>
+        <button formAction={signup}>Sign Up</button>
+      </form>
+
+      <GoogleButton />
+    </div>
   );
 }
