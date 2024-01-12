325c329876da602f2b0a95d3da5e9d71b805e4f7
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2024-01-13 00:19:31 +0100

    Update Renderer.tsx

--- src/components/3D/Renderer.tsx
+++ src/components/3D/Renderer.tsx
@@ -2,11 +2,19 @@
 
 import styles from "@styles/Renderer.module.scss";
 import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
 
 const Renderer = ({ children }: { children?: JSX.Element | JSX.Element[] }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClick = () => {
+    if (isOpen) return;
+    setIsOpen(true);
+  };
+
   return (
-    <div className={styles.canvas}>
-      <Canvas style={{ width: "100%", height: "100%" }} camera={{ position: [0, 2, 4] }} shadows>
+    <div className={isOpen ? styles.canvasOpen : styles.canvas} onClick={() => handleClick()}>
+      <Canvas camera={{ position: [0, 2, 4] }} shadows>
         {children}
       </Canvas>
     </div>
