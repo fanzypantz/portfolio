91fb4c0a3d71f758603880d16433b38124166159
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2024-01-13 00:20:58 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import ChessGame from "@components/Chess/ChessGame";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home({}) {
+  const cookieStore = cookies();
+  const supabase = createServerComponentClient({ cookies: () => cookieStore });
+  const result = await supabase.from("profiles").select("*");
+
+  console.log("result : ", result);
+
   return (
     <main className={styles.main}>
       <ChessGame />
