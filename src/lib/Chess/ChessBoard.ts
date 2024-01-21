1b2318aedcb948dad1ae18e67acedeab71f5c18a
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2024-01-21 21:12:57 +0100

    feat: init board from database(pieces matrix)

--- src/lib/Chess/ChessBoard.ts
+++ src/lib/Chess/ChessBoard.ts
@@ -8,15 +8,15 @@ import Bishop from "@lib/Chess/Bishop";
 import Knight from "@lib/Chess/Knight";
 import Queen from "@lib/Chess/Queen";
 import King from "@lib/Chess/King";
+import { Tables } from "@supabase/database.types";
+import { createPiecesAction } from "@components/BoardGame/actions/createPieces";
 
 class ChessBoard extends AbstractBoard {
-  constructor() {
-    super(8, 8);
-
-    this.initBoard();
+  constructor(game_id: number) {
+    super(8, 8, game_id);
   }
 
-  initBoard(): void {
+  async initBoard(): Promise<void> {
     // Initialize the board with the pieces in their starting positions
     for (let i = 0; i < this.width; i++) {
       this.addPiece(new Pawn(PieceColor.White, new BoardPosition(i, 1)));
@@ -48,6 +48,54 @@ class ChessBoard extends AbstractBoard {
     // Initialize the kings
     this.addPiece(new King(PieceColor.White, new BoardPosition(4, 0)));
     this.addPiece(new King(PieceColor.Black, new BoardPosition(4, 7)));
+
+    // TODO think about moving this
+    // Initialize the pieces in the database
+    const pieces = this.boardPieces.map((piece) => {
+      return {
+        game_id: this.game_id,
+        x_coordinate: piece.position.x,
+        y_coordinate: piece.position.y,
+        color: piece.color,
+        type: piece.type
+      };
+    });
+
+    const { data, error } = await createPiecesAction(this.game_id, pieces);
+
+    console.log("initiated database : ", data);
+  }
+
+  async initBoardFromDatabase(pieces: Tables<"pieces">[]): Promise<void> {
+    // Initialize the board with the pieces in their starting positions
+    for (const piece of pieces) {
+      const pieceColor = piece.color === "White" ? PieceColor.White : PieceColor.Black;
+      const pieceType = piece.type;
+      const piecePosition = new BoardPosition(piece.x_coordinate || 0, piece.y_coordinate || 0);
+
+      switch (pieceType) {
+        case "Pawn":
+          this.addPiece(new Pawn(pieceColor, piecePosition));
+          break;
+        case "Rook":
+          this.addPiece(new Rook(pieceColor, piecePosition));
+          break;
+        case "Bishop":
+          this.addPiece(new Bishop(pieceColor, piecePosition));
+          break;
+        case "Knight":
+          this.addPiece(new Knight(pieceColor, piecePosition));
+          break;
+        case "Queen":
+          this.addPiece(new Queen(pieceColor, piecePosition));
+          break;
+        case "King":
+          this.addPiece(new King(pieceColor, piecePosition));
+          break;
+        default:
+          throw new Error("Invalid piece type");
+      }
+    }
   }
 }
 
