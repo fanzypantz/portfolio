ac779f697d9d47b5fd93c9cba4e121f2b25c2bc3
Andreas Halvorsen Tollånes <dm@andreas-tollanes.com>
2024-01-28 16:06:09 +0100

    Update getUserProfile.ts

--- src/components/Auth/actions/getUserProfile.ts
+++ src/components/Auth/actions/getUserProfile.ts
@@ -3,7 +3,7 @@
 import { supabaseServerActionClient } from "@lib/Auth/supabaseServerAction";
 
 export const getUserProfileAction = async (user_id: string) => {
-  const { data, error } = await supabaseServerActionClient().from("profiles").select("*").eq("id", user_id).single();
+  const { data, error } = await supabaseServerActionClient().from("profiles").select().eq("id", user_id).single();
 
   if (error) {
     return { error };
